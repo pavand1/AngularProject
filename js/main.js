f31e6cb21cc7fbc3c86f0a7493897255a01877d3
Pavan Divekar <pavandivekar@Pavans-MacBook-Air.local>
2017-11-01 00:44:40 +0530

    controller added to model and view

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
 console.log("main.js loaded");
 (function() {
+	"use script";
 	console.log("function called");
-	angular.module("myFirstApp", []);
+	angular.module("myFirstApp", [])
+	.controller("myController", function($scope){
+		console.log("controller is bound to the view");
+		$scope.name = "Pavan";
+	});
 })();
